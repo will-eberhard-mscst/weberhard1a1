25b05fb410c8fc3dc1955081213f69e88e440282
Will Eberhard <willeberhard@hotmail.com>
2016-01-23 20:13:34 -0600

    Initial commit

---
+++ weberhard1a1/src/weberhard1a1/Mainform.java
@@ -0,0 +1,50 @@
+package weberhard1a1;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+
+public class Mainform extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Mainform frame = new Mainform();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Mainform() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JLabel lblPoop = new JLabel("Poop");
+		contentPane.add(lblPoop, BorderLayout.NORTH);
+		
+		JButton btnPoopverb = new JButton("Poop (Verb)");
+		contentPane.add(btnPoopverb, BorderLayout.WEST);
+	}
+
+}
